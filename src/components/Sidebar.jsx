bf920c0247d626c47946e07460f8122baec81cae
Abdelwahab <ab28fb@gmail.com>
2022-08-16 06:25:01 +0100

    WIP sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,52 @@
+import styled from "styled-components";
+import { BsTelegram, BsReddit, BsYoutube, BsWallet2 } from "react-icons/bs";
+import { IoLogoTwitter } from "react-icons/io";
+
+const Container = styled.div`
+  position: fixed;
+  top: 20%;
+  margin-left: 2rem;
+`;
+const Wrapper = styled.div`
+  & p {
+    display: none;
+  }
+`;
+
 const Sidebar = () => {
-  return <h1>Sidebar</h1>;
+  return (
+    <Container>
+      <Wrapper>
+        <div>
+          <BsTelegram />
+        </div>
+        <p>Telegram</p>
+      </Wrapper>
+      <Wrapper>
+        <div>
+          <IoLogoTwitter />
+        </div>
+        <p>Twitter</p>
+      </Wrapper>
+      <Wrapper>
+        <div>
+          <BsReddit />
+        </div>
+        <p>Reddit</p>
+      </Wrapper>
+      <Wrapper>
+        <div>
+          <BsYoutube />
+        </div>
+        <p>Youtube</p>
+      </Wrapper>
+      <Wrapper>
+        <div>
+          <BsWallet2 />
+        </div>
+        <p>Buy</p>
+      </Wrapper>
+    </Container>
+  );
 };
 export default Sidebar;
